f00897938b5366298fd6903cda4aad6389762b02
Badr Elyatim <flipbadr7@gmail.com>
2023-10-31 13:06:25 +0100

    version 1.0.0

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const blocks = document.querySelectorAll(".game-blocks .block")
 
 let wrongTries = 0
+let correctCounter = 0
 const wrongTriesCounter = document.querySelector('.wrong-tries-counter')
 
 let firstGuess = null
@@ -16,8 +17,12 @@ document.querySelector('.start .start-btn').addEventListener('click', startGame)
 
 
 // listening for click event for each game block
-blocks.forEach(block => {
+blocks.forEach((block, index) => {
     block.addEventListener('click', (e) => { guess(e.target, block) })
+
+    let order = Array.from(Array(blocks.length).keys())
+    order = shuffle(order)
+    block.style.order = order[index]
 })
 
 const guess = (target, block) => {
@@ -36,17 +41,16 @@ const flipFace = (block, toOriginal = false) => {
 
     // flip the block
     if (!toOriginal) {
-        frontFace.classList.add('flip-front');
-        backFace.classList.remove('flip-back');
+        frontFace.classList.add('flip-face');
+        backFace.classList.remove('flip-face');
         return
     }
 
-    frontFace.classList.remove('flip-front');
-    backFace.classList.add('flip-back');
+    frontFace.classList.remove('flip-face');
+    backFace.classList.add('flip-face');
 }
 
 const checkChoise = (block) => {
-    const frontFace = block.children[0]
     const backFace = block.children[1]
 
     const guess = backFace.children[0].src
@@ -63,6 +67,30 @@ const checkChoise = (block) => {
                flipFace(secondGuess, true)
             }, 2000, firstGuess, secondGuess)
         }
+        else {
+            correctCounter++
+            if (correctCounter == blocks.length / 2) {
+                document.querySelector('.finish').classList.remove('hidden')
+            }
+        }
         firstGuess = secondGuess = null
     }
 }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
